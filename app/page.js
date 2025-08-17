841d922a6cd03ec682638b14c3816e2767c72020
Abdul Haseeb Qureshi <abluseeb@gmail.com>
2025-08-17 07:27:36 +0500

    design changes

--- app/page.js
+++ app/page.js
@@ -2,102 +2,54 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              app/page.js
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <>
+      <div className="flex justify-center flex-col items-center text-white h-[60vh] gap-4">
+        <div className="text-7xl font-bold flex items-center">Fundora<span><Image src="/alien-21107.gif" alt="logo" width={50} height={50} /></span></div>
+        <p>
+          Join the community of creators and supporters. Start your journey
+          today.
+        </p>
+        <div>
+          <button
+            type="button"
+            className="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
           >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+            Start Here
+          </button>
+          <button
+            type="button"
+            className="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
           >
-            Read our docs
-          </a>
+            Read More
+          </button>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+      </div>
+      <div className="bg-white h-2 opacity-10"></div>
+      <div className="text-white container mx-auto my-15 " >
+        <h1 className="text-center my-4 text-3xl    font-bold " >You can support your favorite creators</h1>
+        <div className="flex justify-around my-8 mt-12 gap-5 items-center" >
+          <div className="item w-[20%] space-y-3 flex flex-col items-center">
+        <Image className=" bg-gray-800 p-2 rounded-full " src="/cash-289_512.gif" alt="" width={80} height={80} />
+        <p>Fund Yourself</p>
+          </div>
+          <div className="item w-[20%] space-y-3 flex flex-col items-center">
+        <Image className=" bg-gray-800 p-2 rounded-full " src="https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExbG9ncHBzMmxiM2FsNThuZW9ycXZjd2d3OWQwdGhreHp0bGJ3ZzBqdyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9cw/fsLU6qu7OddhGHDvso/giphy.gif" alt="" width={80} height={80} />
+        <p className="text-center w-[70%]    " >Built around fans & creators</p>
+          </div>
+          <div className="item w-[20%] space-y-3 flex flex-col items-center">
+        <Image className=" bg-gray-800 p-2 rounded-full " src="/spring-20172_512.gif" alt="" width={80} height={80} />
+        <p className="text-center w-[70%] " >Turn passion into growth</p>
+          </div>
+                    
+        
+
+        </div>
+      </div>
+      <div className="bg-white h-2 opacity-10"></div>
+      <div className="text-white container mx-auto flex justify-center flex-col items-center my-8 " >
+        <h2 className="text-center my-4 text-3xl font-bold mb-8 " >Learn More about Fundora</h2>
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/QtaorVNAwbI?si=ULaZorodtiXCaFWw" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
+      </div>
+    </>
   );
 }
---
+++ components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer className='bg-gray-800 text-white px-4 h-16 flex items-center justify-center '>
+        <p className='text-center'>© 2025 Patreon Clone by Abdul Haseeb Qureshi</p>
+    </footer>
+  )
+}
+
+export default Footer
