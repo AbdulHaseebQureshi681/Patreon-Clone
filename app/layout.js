95dcd0ca3f1d720ec7ddb0c49f6fa46143635637
Abdul Haseeb Qureshi <abluseeb@gmail.com>
2025-08-25 21:40:39 +0500

    convex setup done aahhh

--- app/layout.js
+++ app/layout.js
@@ -3,6 +3,7 @@ import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import SessionWrapper from "@/components/SessionWrapper";
+import { ConvexClientProvider } from "@/components/ConvexClientProvider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,8 +29,9 @@ export default function RootLayout({ children }) {
         <SessionWrapper>
         <Navbar />
         <div className="flex-1 [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]">
-        
-          {children}
+          <ConvexClientProvider>
+            {children}
+          </ConvexClientProvider>
         </div>
         <Footer />
         </SessionWrapper>
